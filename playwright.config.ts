736e6fefefc70f3efd38c354d6b7cbe32f43e8b6
SIPAHTSANAN KITTITHANATKUN <81869508+sipahtsanan@users.noreply.github.com>
2026-02-19 14:49:06 +0700

    edit files to run on pipeline

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,5 +1,8 @@
 import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
-  reporter: [['html', { open: 'never' }]],
+    reporter: [
+        ['html', { open: 'never' }],
+        ['junit', { outputFile: 'test-results/results.xml' }]
+    ],
 });
